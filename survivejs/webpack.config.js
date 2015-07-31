54352936dec8635f772070da0069d1d9249d5f16
Alexey Nikiforov <Avetome@gmail.com>
2015-07-31 22:07:35 +0400

    react introduce

--- survivejs/webpack.config.js
+++ survivejs/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
@@ -7,12 +8,20 @@ var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
     entry: [path.resolve(ROOT_PATH, 'app/main')],
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
     output: {
         path: path.resolve(ROOT_PATH, 'build'),
         filename: 'bundle.js'
     },
     module: {
         loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel?stage=1',
+                include: path.resolve(ROOT_PATH, 'app')
+            },
         {
             test: /\.css$/,
             loaders: ['style', 'css']
@@ -25,13 +34,38 @@ var common = {
 };
 
 if(TARGET === 'build') {
-  module.exports = merge(common, {
-    devtool: 'source-map'
-  });
+    module.exports = merge(common, {
+        devtool: 'source-map',
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.DefinePlugin({
+                  'process.env': {
+                      // This has effect on the react lib size
+                      'NODE_ENV': JSON.stringify('production')
+                  }
+            }),
+        ]
+    });
 }
 
 if(TARGET === 'dev') {
-  module.exports = merge(common, {
-    devtool: 'eval'
-  });
+    module.exports = merge(common, {
+        devtool: 'eval',
+        entry: [
+            'webpack/hot/dev-server'
+        ],
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: ['react-hot', 'babel?stage=1'],
+                    include: path.resolve(ROOT_PATH, 'app'),
+                },
+            ],
+        }
+    });
 }
